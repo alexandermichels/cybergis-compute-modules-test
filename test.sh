e94a470f8eed344b394201d500ee851208e37d3c
alexandermichels <michels9@illinois.edu>
2021-09-09 10:12:23 -0500

    trying to run conda

--- test.sh
+++ test.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 source /etc/profile.d/z00_lmod.sh
-module use /data/cigi/cybergisx-easybuild/easybuild/modules/all
-module use /data/cigi/cybergisx-easybuild/metamodules
+export CIGI_EB_ROOT="/data/cigi/cybergisx-easybuild"
+module use $CIGI_EB_ROOT/easybuild/modules/all
+module use $CIGI_EB_ROOT/metamodules
 module avail
 module load cybergisx
 module list
 echo $PATH
 echo $LD_LIBRARY_PATH
+conda activate $CIGI_EB_ROOT/conda/cybergisx-py3-2021-08-13
+python -c "import networkx"
