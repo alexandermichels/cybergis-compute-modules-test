d0d55c5a00ed360c75708b22d8406969d4919d77
alexandermichels <michels9@illinois.edu>
2021-09-09 10:00:26 -0500

    :tada: init commit

---
+++ test.sh
@@ -0,0 +1,7 @@
+module use /data/cigi/cybergisx-easybuild/easybuild/modules/all
+module use /data/cigi/cybergisx-easybuild/metamodules
+module avail
+module load cybergisx
+module list
+echo $PATH
+echo $LD_LIBRARY_PATH
